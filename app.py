f72eaf40f5eed5305aea89c0a7bca7172b7ff45d
Zhipeng Peng <wingzpeng83@gmail.com>
2023-06-25 13:36:49 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request, send_from_directory
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    # Get the uploaded file
+    file = request.files['image']
+    filename = file.filename
+
+    # Generate the result filename
+    result_filename = filename.replace('.jpg', '-result.jpg')
+    result_filepath = os.path.join('data', result_filename)
+
+    if os.path.exists(result_filepath):
+        # Return the result image filename to be displayed in the web page
+        return render_template('result.html', filename=result_filename)
+
+    # If the result file doesn't exist, display an error message
+    error_message = f"No related image found for {filename}."
+    return render_template('index.html', error_message=error_message)
+
+@app.route('/data/<path:filename>')
+def download_file(filename):
+    # Serve the image file
+    return send_from_directory('data', filename)
+
+if __name__ == '__main__':
+    app.run(debug=True)
